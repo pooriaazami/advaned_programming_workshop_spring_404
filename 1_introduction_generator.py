dd32a86671860d4b7390a4787da9891207eeb2c7
Pooria Azami <pooriaazami@gmail.com>
2025-04-09 10:15:55 +0330

    initial coomit

---
+++ 1_introduction_generator.py
@@ -0,0 +1,26 @@
+import time
+
+def is_prime(number):
+    if number < 2:
+        return False
+
+    lim = int(number ** .5) + 1
+    for i in range(2, lim):
+        if number % i == 0:
+            return False
+
+    return True
+
+
+def primes_less_than_input(n):
+    # answer = []
+    for i in range(2, n + 1):
+        if is_prime(i) == True:
+            time.sleep(1)
+            yield i
+            # answer.append(i)
+
+    # return answer
+
+for x in primes_less_than_input(100):
+    print(x)
