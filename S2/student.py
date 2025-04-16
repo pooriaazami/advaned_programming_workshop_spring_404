806acd13fb133b18456b265dcff61f3e2b85c4fb
Pooria Azami <pooriaazami@gmail.com>
2025-04-16 15:48:04 +0330

    S2

---
+++ S2/student.py
@@ -0,0 +1,49 @@
+class Student:
+    def __init__(self, name, family, age):
+        self.__name = name
+        self.family = family
+        self.__age = age
+
+    @property
+    def name(self):
+        print('name(self) called')
+        return self.__name
+
+    @name.setter
+    def name(self, new_value):
+        print('name(self, new_value called)')
+        self.__name = new_value
+
+    @property
+    def age(self):
+        return self.__age
+
+    @age.setter
+    def age(self, new_value):
+        # if new_value >= 0:
+        #     self.__age = new_value
+        # else:
+        #     print('Error')
+
+        assert new_value >= 0
+        self.__age = new_value
+
+
+s = Student('name', 'family', 20)
+print(s.name)
+s.name = 'Another name'
+print(s.name)
+
+print('='*50)
+
+print(s.family)
+s.family = 'Another family'
+print(s.family)
+
+s.age = 10
+print(s.age)
+try:
+    s.age = -10
+except:
+    print('There was an error')
+print(s.age)
